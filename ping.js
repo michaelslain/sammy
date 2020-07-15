607ba96fce000c4eed2e2714ccc71fca812ce84d
michaelslain <misha.slain@icloud.com>
2020-07-15 15:37:28 -0700

    Actually fixed issue

--- ping.js
+++ ping.js
@@ -2,14 +2,18 @@ import { exec } from 'child_process'
 
 const uri = 'https://sammy-discord-bot.herokuapp.com/'
 
-exec(`ping ${uri}`, (err, stdout, stderr) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    if (stderr) {
-        console.error(stderr)
-        return
-    }
-    console.log('PING!')
-})
+const ping = () => {
+    exec(`ping ${uri}`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        if (stderr) {
+            console.error(stderr)
+            return
+        }
+        console.log('PING!')
+    })
+}
+
+setInterval(ping, 29999)
